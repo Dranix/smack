e487dc274b83bc6501a43577b298137bc7a0ad75
Duong Dang Hoang <alex@cpe-101-190-104-71.nb03.nsw.asp.telstra.net>
2018-01-24 18:26:16 +1100

    Login UI

---
+++ smack/smack/Controller/LoginVC.swift
@@ -0,0 +1,26 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Duong Dang Hoang on 24/1/18.
+//  Copyright © 2018 Duong Dang Hoang. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var closeBtn: UIButton!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var signUpBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ smack/smack/Utilities/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  smack
+//
+//  Created by Duong Dang Hoang on 24/1/18.
+//  Copyright © 2018 Duong Dang Hoang. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
