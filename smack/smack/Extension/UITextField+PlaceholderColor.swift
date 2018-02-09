1a17fb682a48829c32e9df2cc33a065b3afbef2c
Duong Dang Hoang <alex@cpe-120-152-49-248.nb03.nsw.asp.telstra.net>
2018-02-09 18:08:31 +1100

    random color

---
+++ smack/smack/Extension/UITextField+PlaceholderColor.swift
@@ -0,0 +1,9 @@
+//
+//  UITextField+PlaceholderColor.swift
+//  smack
+//
+//  Created by Duong Dang Hoang on 9/2/18.
+//  Copyright © 2018 Duong Dang Hoang. All rights reserved.
+//
+
+import Foundation
---
+++ smack/smack/View/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  smack
+//
+//  Created by Duong Dang Hoang on 9/2/18.
+//  Copyright © 2018 Duong Dang Hoang. All rights reserved.
+//
+
+import UIKit
+
+class CircleImage: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
